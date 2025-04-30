e11b36d2c03a4e799c2343c41bc39a318ab2ae87
xiaohan <bornwarm@foxmail.com>
2025-04-30 14:51:00 +0800

    Validating Built-in Boolean Conditions.

--- rpc-demo-consumer/src/main/java/com/rpc/demo/consumer/service/RpcServiceReference.java
+++ rpc-demo-consumer/src/main/java/com/rpc/demo/consumer/service/RpcServiceReference.java
@@ -20,10 +20,10 @@ import java.util.function.Predicate;
 @Component
 public class RpcServiceReference {
 
-    @RpcReference(version = "1.0.0", enableLocalService = true, condition = "time0900-1800")
+    @RpcReference(version = "1.0.0", enableLocalService = true, condition = "booltrue")
     private MatrixService matrixService;
 
-    @RpcReference(version = "1.0.0", enableLocalService = true, condition = "count3")
+    @RpcReference(version = "1.0.0", enableLocalService = true, condition = "boolfalse")
     private TextService textService;
 
     static {
